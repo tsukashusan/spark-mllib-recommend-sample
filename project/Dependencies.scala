e175d457feffca0e7f20bad5579914d028947f75
Shuichi Tsukamoto <shtsukam@microsoft.com>
2017-11-26 19:14:40 +0900

    update Dependencies.scala.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
-  lazy val spakCore = "org.apache.spark" % "spark-core_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
-  lazy val spakSQL = "org.apache.spark" % "spark-sql_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
-  lazy val spakStreaming = "org.apache.spark" % "spark-streaming_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
-  lazy val spakMlib = "org.apache.spark" % "spark-mllib_2.11" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakCore = "org.apache.spark" %% "spark-core" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakSQL = "org.apache.spark" %% "spark-sql" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakStreaming = "org.apache.spark" %% "spark-streaming" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
+  lazy val spakMlib = "org.apache.spark" %%  "spark-mllib" % "2.1.1.2.6.2.3-1" % "provided" exclude("org.apache.zookeeper", "zookeeper")
 }
